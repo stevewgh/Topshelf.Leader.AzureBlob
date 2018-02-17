7f85673694853eb5b94e172c7f1f3896746d9545
stevewgh <stevewgh@users.noreply.github.com>
2018-02-17 01:08:15 +0000

    Fixed unit test

--- src/Topshelf.Leader.AzureBlob.Tests/AzureBlobBuilderExtensionShould.cs
+++ src/Topshelf.Leader.AzureBlob.Tests/AzureBlobBuilderExtensionShould.cs
@@ -1,17 +1,23 @@
 ﻿using Microsoft.WindowsAzure.Storage;
+using Topshelf.Logging;
 using Xunit;
 
 namespace Topshelf.Leader.AzureBlob.Tests
 {
     public class AzureBlobBuilderExtensionShould
     {
+        public AzureBlobBuilderExtensionShould()
+        {
+            HostLogger.UseLogger(new TraceHostLoggerConfigurator());
+        }
+
         [Fact]
         public void register_the_lease_manager_with_the_builder()
         {
             var builder = new LeaseConfigurationBuilder("Node1");
             builder.WithAzureBlobStorageLeaseManager(new BlobSettings(CloudStorageAccount.DevelopmentStorageAccount));
             var built = builder.Build();
-//            Assert.IsType<AzureBlobLeaseManager>(built.);
+            Assert.IsType<AzureBlobLeaseManager>(built.LeaseManager(built));
         }
     }
 }
